ee52e159313111f0e36f96270c6edab83dcb9618
Bulemi Robert Charles <bulemicharlesrobert@gmail.com>
2025-04-15 18:01:03 +0300

    Remove SQLite DB config and use PostgreSQL from Render

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -84,12 +84,6 @@ WSGI_APPLICATION = 'inventory_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 
