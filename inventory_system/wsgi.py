c03f125cf73f4161025ddb2db9ea77994cc6d644
Bulemi Robert Charles <bulemicharlesrobert@gmail.com>
2025-04-15 19:26:54 +0300

    Fix WhiteNoise configuration: correct middleware, update to v6.9.0

--- inventory_system/wsgi.py
+++ inventory_system/wsgi.py
@@ -7,11 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
-from whitenoise import WhiteNoise
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory_system.settings')
+from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+application = WhiteNoise(application, root='staticfiles')
