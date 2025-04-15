c7867182e225a54c9df2c4f7f8d164bf4c0c63ed
Bulemi Robert Charles <bulemicharlesrobert@gmail.com>
2025-04-15 18:51:49 +0300

    Fix WhiteNoise middleware typo

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddlewaredjango',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
