f32cb57aebb89d80578575b115a6c5f171f4948d
Bulemi Robert Charles <bulemicharlesrobert@gmail.com>
2025-04-15 17:37:37 +0300

    Configure PostgreSQL database with Render DATABASE_URL

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -16,10 +16,14 @@ import os
 import dj_database_url
 from decouple import config
 
-
+import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
