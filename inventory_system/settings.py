eabc8c8a3fd9968d865531befdbd1a245233a093
Bulemi Robert Charles <bulemicharlesrobert@gmail.com>
2025-04-15 17:50:07 +0300

    Switched to PostgreSQL and ran migrations

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -19,11 +19,7 @@ from decouple import config
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
